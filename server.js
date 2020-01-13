21f4b107882a470ff907759cdc65efc8d88f2cdb
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2020-01-13 15:54:12 +0100

    get values from spreadsheet

--- server.js
+++ server.js
@@ -5,23 +5,29 @@ var server = app.listen(port);
 var socket = require('socket.io');
 var io = socket(server);
 
-var google = require('googleapis');
+var {google} = require('googleapis');
 var credentials = require('./spreadsheet-credentials.json');
 var auth = new google.auth.JWT(
   credentials.client_email,
   null,
-  credentials.pivate_key,
+  credentials.private_key,
   [
     'https://www.googleapis.com/auth/spreadsheets'
   ],
   null
 );
 
+var pulls = 0;
+
 google.options({auth});
-
-var GoogleSpreadsheet = require('google-spreadsheet');
-
-var doc = new GoogleSpreadsheet('1Q25gnGC5R3uE4qQHON8njArLOeIcu0IvrbT1jTqy5QQ');
+var sheets = google.sheets('v4');
+var spreadsheetId = '1Q25gnGC5R3uE4qQHON8njArLOeIcu0IvrbT1jTqy5QQ';
+sheets.spreadsheets.values.get({
+  spreadsheetId,
+  range: 'swordInStoneData!B1'
+}, (err, result) => {
+  console.log(result.data.values);
+});
 
 app.use(express.static('public'));
 
