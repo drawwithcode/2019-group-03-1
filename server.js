091c8b2ac90d0e59d4e3d69334a7d80cfb6f04a2
DoksaVPC <55451950+DoksaVPC@users.noreply.github.com>
2020-01-13 13:34:48 +0100

    google sheets credentials

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var server = app.listen(port);
 var socket = require('socket.io');
 var io = socket(server);
 
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('./spreadsheet-credentials.json');
+var doc = new GoogleSpreadsheet('1Q25gnGC5R3uE4qQHON8njArLOeIcu0IvrbT1jTqy5QQ');
+
 app.use(express.static('public'));
 
 io.on('connection', newConnection);
@@ -12,3 +16,15 @@ io.on('connection', newConnection);
 function newConnection(socket) {
   //socket code here
 }
+
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(1, function (err, rows) {
+
+    console.log(rows);
+
+  });
+
+});
